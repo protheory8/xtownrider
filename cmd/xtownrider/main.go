93ea5a0c4cce0abeefc87710f0c1986988edae73
protheory8 <editaw5@gmail.com>
2020-04-18 10:31:44 +0000

    Add copyright notice to terminal output

--- cmd/xtownrider/main.go
+++ cmd/xtownrider/main.go
@@ -18,9 +18,19 @@
 
 package main
 
-import "github.com/protheory8/xtownrider/internal/app/xtownrider"
+import (
+	"fmt"
+
+	"github.com/protheory8/xtownrider/internal/app/xtownrider"
+)
 
 func main() {
+	fmt.Println("Xtownrider Copyright © 2020 The Xtownrider Contributors")
+	fmt.Println("This program comes with ABSOLUTELY NO WARRANTY.")
+	fmt.Println("This is free software, and you are welcome to redistribute it")
+	fmt.Println("under certain conditions. Read file 'COPYING' for details.")
+	fmt.Println()
+
 	gameState := xtownrider.NewGameState()
 	defer gameState.Drop()
 
