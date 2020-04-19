0d302cdc892e14485fa2b6bcd1aeab15cfcc982b
protheory8 <editaw5@gmail.com>
2020-04-19 17:39:51 +0500

    Revert "Change print messages in main function"
    This reverts commit 4ad416ae69307f85571a0a1d5a7b3efe2ae35fc2.

--- cmd/xtownrider/main.go
+++ cmd/xtownrider/main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
-	fmt.Println("Copyright 2020 The Xtownrider Contributors")
+	fmt.Println("Xtownrider Copyright © 2020 The Xtownrider Contributors")
+	fmt.Println("This program comes with ABSOLUTELY NO WARRANTY.")
+	fmt.Println("This is free software, and you are welcome to redistribute it")
+	fmt.Println("under certain conditions. Read file 'COPYING' for details.")
 	fmt.Println()
 
 	xtownrider.MainGameLoop()
