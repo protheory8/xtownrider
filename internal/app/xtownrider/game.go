29544684f93bba1957989c575985d47a35f8ef5c
protheory8 <editaw5@gmail.com>
2020-04-19 12:37:15 +0000

    Remove license and move files into other project

--- internal/app/xtownrider/game.go
+++ internal/app/xtownrider/game.go
@@ -1,20 +1,6 @@
-// Copyright 2020 The Xtownrider Contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package xtownrider
 
-import "github.com/protheory8/xtownrider/pkg/libxtownrider"
+import "github.com/protheory8/goalengine"
 
 const (
 	// WindowTitle is a constant that stores the name of the window.
@@ -29,24 +15,24 @@ const (
 func MainGameLoop() {
 	gameState := NewGameState()
 	defer gameState.Drop()
-	entityManager := libxtownrider.NewEntityManager()
+	entityManager := goalengine.NewEntityManager()
 
-	entityManager.AddEntity(libxtownrider.NewEntityBuilder().
+	entityManager.AddEntity(goalengine.NewEntityBuilder().
 		AddSprite(gameState.Renderer, "resources/resource.bmp").
 		SetLocation(400, 150).
 		Build())
 
-	entityManager.AddEntity(libxtownrider.NewEntityBuilder().
+	entityManager.AddEntity(goalengine.NewEntityBuilder().
 		AddSprite(gameState.Renderer, "resources/resource.bmp").
 		SetLocation(100, 150).
 		Build())
 
-	entityManager.AddEntity(libxtownrider.NewEntityBuilder().
+	entityManager.AddEntity(goalengine.NewEntityBuilder().
 		AddSprite(gameState.Renderer, "resources/resource.bmp").
 		SetLocation(300, 400).
 		Build())
 
 	for {
-		libxtownrider.RenderSystem(entityManager.GetEntities(), gameState.Renderer)
+		goalengine.RenderSystem(entityManager.GetEntities(), gameState.Renderer)
 	}
 }
--- internal/app/xtownrider/gamestate.go
+++ internal/app/xtownrider/gamestate.go
@@ -1,21 +1,7 @@
-// Copyright 2020 The Xtownrider Contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package xtownrider
 
 import (
-	"github.com/protheory8/xtownrider/pkg/libxtownrider"
+	"github.com/protheory8/goalengine"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -53,7 +39,7 @@ func NewGameState() *GameState {
 
 // Drop invokes Destroy on every SDL component.
 func (gameState *GameState) Drop() {
-	libxtownrider.Log(libxtownrider.LogTypeDebug, "Dropping GameState...")
+	goalengine.Log(goalengine.LogTypeDebug, "Dropping GameState...")
 
 	if gameState.Window != nil {
 		gameState.Window.Destroy()
